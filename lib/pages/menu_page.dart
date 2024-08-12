91bd34014ecfaed797e9628fc9dc4250464107d3
Nini Gedeshuri <ninigedeshuri@Giorgis-MacBook-Air.local>
2024-08-12 14:26:48 +0400

    add

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -11,7 +11,7 @@ class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Color.fromARGB(255, 219, 212, 211),
+      backgroundColor: const Color.fromARGB(255, 219, 212, 211),
       appBar: AppBar(
         backgroundColor:Colors.transparent,
         elevation: 0,
