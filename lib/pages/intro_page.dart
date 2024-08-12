58a366190017d7f337f84759499812a7391d1247
Nini Gedeshuri <ninigedeshuri@Giorgis-MacBook-Air.local>
2024-08-12 13:54:45 +0400

    add

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sushi_app/components/button.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -12,17 +13,39 @@ class IntroPage extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 30),
-            const Text("SUSHI ROOM",
+            const Text(
+              "SUSHI ROOM",
               style: TextStyle(
                 fontSize: 28,
+                fontWeight: FontWeight.w700,
                 color: Colors.white,
               ),
             ),
             const SizedBox(height: 25),
             Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Image.asset('lib/images/sushi_set.png')
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset('lib/images/sushi_set.png')),
+            const  Text (
+                  "THE TASTE OF JAPANECE FOOD",
+                  style: TextStyle(
+                    fontSize: 37,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              
+            
+            const SizedBox(height: 25),
+            const Text(
+              "the real taste of the most popular Japanice foot from anywhere and anytime",
+              style: TextStyle(
+                fontSize: 16,
+                height: 1.5,
+                color: Color.fromARGB(255, 124, 124, 124),
+              ),
             ),
+            const SizedBox(height: 100),
+            const MyButton(text: "Get Started")
           ],
         ),
       ),
