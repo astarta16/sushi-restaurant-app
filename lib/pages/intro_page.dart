b1cc17ca84d41f74c45ab32118cb2b9144e50130
Nini Gedeshuri <ninigedeshuri@Giorgis-MacBook-Air.local>
2024-08-12 12:05:10 +0400

    make starter files

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return const  Scaffold(
+    backgroundColor: const Color.fromARGB(255, 97, 15, 9),
+    body: Column(children: [
+
+    ],),
+  );
+  }
+}
