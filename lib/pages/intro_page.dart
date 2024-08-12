266e7adbc7609c80b2af6465f3f28fb6504f9e20
Nini Gedeshuri <ninigedeshuri@Giorgis-MacBook-Air.local>
2024-08-12 12:28:27 +0400

    add

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,17 +1,31 @@
-
 import 'package:flutter/material.dart';
-
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-  return const  Scaffold(
-    backgroundColor: const Color.fromARGB(255, 97, 15, 9),
-    body: Column(children: [
-
-    ],),
-  );
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 97, 15, 9),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            const Text("SUSHI ROOM",
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('lib/images/sushi_set.png')
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
