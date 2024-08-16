17244bb6e83cfef433f834a7f673ab7c3e8f19ea
Nini Gedeshuri <ngedeshuri2001@gmail.com>
2024-08-16 12:36:13 +0400

    add

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sushi_app/components/button.dart';
+import 'package:sushi_app/theme/colors.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -10,16 +12,58 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: const Color.fromARGB(255, 219, 212, 211),
       appBar: AppBar(
-        backgroundColor:Colors.transparent,
+        backgroundColor: Colors.transparent,
         elevation: 0,
-        leading:Icon(Icons.menu, 
-        color: Colors.grey[900],),
-        title: Text("Tokyo", style: TextStyle(color: Colors.grey[900]),),
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[900],
         ),
+        title: Text(
+          "Tokyo",
+          style: TextStyle(color: Colors.grey[900]),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Get 32% Promo',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 10), 
+                    MyButton(
+                      text: "Redeem",
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  'lib/images/sushi_stick.png',
+                  height: 90.0,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
-  
   }
 }
